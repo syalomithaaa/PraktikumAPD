129767ef25e0cbd730533670875838cb80a1ac12
syalomithaaa <syalomithachello1111@gmail.com>
2024-09-25 16:21:16 +0800

    Add files via upload

---
+++ test4.py
@@ -0,0 +1,59 @@
+# ulang = 0
+# for i in range (ulang):
+#    .print("hallo")\
+
+# simpan = [12, "udin petot", 14.5, True, 'A',"102"]
+# for i in simpan[2:5]:
+#     print(i)
+# print(simpan)
+# print(simpan[4])
+
+# print("menu rumah makan informatika 24: ")
+# print("--------------------------------")
+# simpan = ["nasi goreng", "mie goreng", "mie ayam", "bakso", "soto"]
+# for i in simpan:
+#     print(i)
+
+# print("menu rumah makan informatika 24: ")
+# print("--------------------------------")
+# simpan = ["nasi goreng", "mie goreng", "mie ayam", "bakso", "soto"]
+# for i, menu in enumerate(simpan,start=100):
+#     print(f"{i}.{menu}")
+
+# print("menu rumah makan informatika 24: ")
+# print("--------------------------------")
+# simpan = ["nasi goreng", "mie goreng", "mie ayam", "bakso", "soto"]
+# for i in range(len(simpan)):
+#     print(f"{i+1}. {simpan[i]}")
+
+
+# print("menu rumah makan informatika 24: ")
+# print("--------------------------------")
+# simpan = ["nasi goreng", "mie goreng", "mie ayam", "bakso", "soto"]
+# for i in range(len(simpan)):
+#     print(f"{i+1}. {simpan[i]}")
+
+# makanan = ["mie","sop","bakso"]
+# minuman = ["es teh","air putih","es jeruk"]
+
+# for i in makanan:
+
+
+# print(f"total perulangan: {hitung}")
+
+
+hitung = 0
+While True:
+    hitung += 1
+    ulang = input("masi ingin lanjut? ")
+    if ulang == "y" or ulang =="y":
+       print("oke kita lanjut")
+       continue
+
+# print(f"total perulangan: {hitung}")
+
+# print("daftar bilangan ganjil dari 1-10")
+# for i in a range(10):
+#     if i % 2 == 0:
+#        continue
+#     print(1)
